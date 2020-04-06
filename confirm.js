b5fa43abd0534960b7919b353426798831bb41f4
unknown <essidbelhassan@gmail.com>
2020-04-06 23:43:00 +0100

    about me

--- confirm.js
+++ confirm.js
@@ -1,7 +1,6 @@
 document.getElementById ( "ps" ).style.visibility = "hidden";
 var rep = prompt ( "This personal section conatins photos if you wish to see them, you must type the password below," +
-    "otherwise visit the 'Contact Me' section and contact belhassen." , "Type The Password Here" );
-var pw = "mandarine";
+    "otherwise visit the 'Contact Me' section and contact belhassen." , "Type The Password Here" );let pw="mandarine";
 if ( rep === pw ) {
     var cf = confirm ( "Welcome\n1#Please keep the password secret\n2#you are not allowed to use or download this " +
         "images or to change the page code under any circumstances without consulting the owner.\n" +
