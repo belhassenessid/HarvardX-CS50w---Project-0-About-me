cd7ec95a4a5b5e4158ac349c33e2a1e5e2e452fb
unknown <essidbelhassan@gmail.com>
2020-04-08 16:55:43 +0100

    about me

--- confirm.js
+++ confirm.js
@@ -1,11 +1,7 @@
 var rep = prompt ( "This personal section conatins photos if you wish to see them, you must type the password below," +
     "otherwise visit the 'Contact Me' section and contact belhassen." , "Type The Password Here" );let pw="test";
-if ( rep === pw ) {
-    document.getElementById ( "ps" ).style.visibility = "visible";
-}
-else {
+if (!(rep===pw)){
     window.location.href = "Contact%20Me.html";
-    alert( "ERROR 401\nWrong password" );
-}
+    alert( "ERROR 401\nWrong password" );}
 let d = new Date;
 document.getElementById ( "date" ).innerHTML = d.getFullYear ();
